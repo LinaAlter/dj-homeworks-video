a41abf931183928d2517c9f0f9c16bf1428ddfb4
Lina Volkova <anotherwaygirl@yandex.ru>
2024-12-02 19:02:00 +0700

    2.1-database homework

--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -1,6 +1,17 @@
 from django.db import models
-
+from django.template.defaultfilters import slugify
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=80)
+    image = models.URLField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
+    release_date = models.DateField(auto_now=False, auto_now_add=False)
+    lte_exists = models.BooleanField(null=False)
+    slug = models.SlugField(max_length = 80, unique=True)
+    
+    def __str__(self):
+        return f'{self.name}; {self.price}; {self.image}; {self.release_date}; {self.lte_exists}; {self.slug}'
+    
+    def save(self,  *args, **kwargs):
+        self.slug = slugify(self.title)
+        return super(Phone, self).save(*args, **kwargs)
--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
@@ -7,11 +7,21 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    sort_by = request.GET.get('sort', 'name')
+    if sort_by == 'min_price':
+        all_phones = Phone.objects.order_by('price')
+    elif sort_by == 'max_price':
+        all_phones = Phone.objects.order_by('-price')
+    elif sort_by == 'name':
+        all_phones = Phone.objects.order_by('name')
+    else:
+        all_phones = Phone.objects.all()        
+    context = {'phones': all_phones}
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    phone = Phone.objects.filter(slug__contains=slug).first()
+    context = {'phone': phone}
     return render(request, template, context)
