f63181c1cdfe31ba3318a1f7b2c6eee39b341785
Lina Volkova <anotherwaygirl@yandex.ru>
2024-12-09 21:44:32 +0700

    homework orm-2

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+from .models import Article, Scope, Tag
 
-from .models import Article
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if 'is_main' in form.cleaned_data:
+                if form.cleaned_data['is_main']:
+                    count += 1
+                    if count > 1:
+                        raise ValidationError('Основной тэг может быть только один.')
+            if count == 0:
+                raise ValidationError('Основной тег не назначен.')
+            else:
+                pass
+        return super().clean()
 
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    formset = (ScopeInlineFormset)
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ScopeInline]
+    
+
